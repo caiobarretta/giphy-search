366bd04673270a956b13fc36ec1597a52ec211d2
Caio Augusto Barretta <caioabarretta@gmail.com>
2019-01-20 18:01:46 -0200

    Criando giphy-search-manual Seção 4, aula 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GifSeachManualComponent } from './giphy/giphy-search-manual/giphy-search-manual.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GifSeachManualComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/giphy/giphy-search-manual/giphy-search-manual.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'giphy-search-manual',
+  templateUrl: 'giphy-search-manual.component.html'
+})
+
+@NgModule({
+})
+
+export class GifSeachManualComponent implements OnInit {
+  constructor() {
+   }
+
+  ngOnInit() {
+  }
+}
