7817451c27915833b81c84221bde2e9756663700
Caio Augusto Barretta <caioabarretta@gmail.com>
2019-01-23 21:53:20 -0200

    Teste do GiphyService - mokando Http com Jasmine Seção 5, aula 34

--- src/app/giphy/giphy-search.service.ts
+++ src/app/giphy/giphy-search.service.ts
@@ -11,7 +11,12 @@ export class GiphySearchService {
 
   pesquisarGiphy(limit: string, term: string): Observable<Giphy> {
     const apiKey = 'mdYTsHMSGbIyGff5ZP4xrKEdXWb1P9V1';
-    const url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=' + apiKey + '&limit=' + limit;
+    const url = this.getUrl(apiKey, limit, term);
     return this.http.get<Giphy>(url);
+  }
+
+  getUrl(apiKey: string, limit: string, term: string): string {
+
+    return 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=' + apiKey + '&limit=' + limit;
   }
 }
--- src/app/giphy/test/giphy-search.service.spec.ts
+++ src/app/giphy/test/giphy-search.service.spec.ts
@@ -1,14 +1,35 @@
+import { GiphySearchService } from '../giphy-search.service';
+
 describe('giphy-search.service.spec.ts', () => {
+
+  const httpMock = jasmine.createSpyObj('Http', ['get']); // Dublê
+  let service: GiphySearchService;
+
+  // Entradas
+  const limit = '10';
+  const term = 'happy';
+  const apiKey = 'mdYTsHMSGbIyGff5ZP4xrKEdXWb1P9V1';
+
+  // Esperado
+  const expected = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=' + apiKey + '&limit=' + limit;
+
   beforeEach(() => {
+    service = new GiphySearchService(httpMock);
   });
 
-  fit('2 deveria ser igual a 2', () => {
-    // Entrada
-    let a = 0;
+  it('pesquisarGiphy', () => {
     // Processamento
-    a = 0 + 1;
+    service.pesquisarGiphy(limit, term);
     // Saída
-    expect(a).toBe(2);
+    expect(httpMock.get).toHaveBeenCalled();
+    expect(httpMock.get).toHaveBeenCalledWith(expected);
+  });
+
+  it('getUrl', () => {
+    // Processamento
+    const url = service.getUrl(apiKey, limit, term);
+    // Saída
+    expect(url).toBe(expected);
   });
 
 });
