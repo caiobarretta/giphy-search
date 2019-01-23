9f696f8309993b5e51a64de9352fbba93556aada
Caio Augusto Barretta <caioabarretta@gmail.com>
2019-01-23 21:13:45 -0200

    Extrutura geral do teste Seção 5, aula 33

---
+++ src/app/giphy/test/giphy-search.service.spec.ts
@@ -0,0 +1,14 @@
+describe('giphy-search.service.spec.ts', () => {
+  beforeEach(() => {
+  });
+
+  fit('2 deveria ser igual a 2', () => {
+    // Entrada
+    let a = 0;
+    // Processamento
+    a = 0 + 1;
+    // Saída
+    expect(a).toBe(2);
+  });
+
+});
