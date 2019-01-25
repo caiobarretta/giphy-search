1db112e3ed992467560432731450ea58051e6c85
Caio Augusto Barretta <caioabarretta@gmail.com>
2019-01-25 14:34:24 -0200

    document, location, window e Location Seção 6, aula 44

--- src/app/pagenotfound/pagenotfound.component.ts
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Location } from '@angular/common'
 @Component({
   selector: 'page-not-found',
   templateUrl: './pagenotfound.component.html'
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) {
+  }
 
   ngOnInit() {
+    console.log('url atual', document.location.href);
+    console.log('location', location.href);
+    // window.open("https://www.google.com/");
+  }
+
+  voltar() {
+    this.location.back();
   }
 
 }
